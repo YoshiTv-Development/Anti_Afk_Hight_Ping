601ec0d3a7ed57f7eca679e8249e4849aa9ecf55
YoshiTv-Development <142677543+YoshiTv-Development@users.noreply.github.com>
2023-08-30 21:16:43 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,6 +4,6 @@ Config.TimeAfk = 600 -- QUESTI SONO 10 MINUTI 600000
 Config.TimeKickPing = 5000
 Config.MaxPing = 20
 Config.PlayerDroppedafk = 'Sei stato disconnesso da %s perché sei stato a lungo inattivo!'
-Config.NameServer = 'Fusion Evolved'
+Config.NameServer = 'NOME SERVER'
 Config.PlayerDroppedping = 'Sei stato disconnesso da %s a causa del tuo ping\nPing Limit: %s\nYour Ping: %s'
   
